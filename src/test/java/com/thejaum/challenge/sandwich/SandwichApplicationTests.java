6add93463f9a502a833f90371056cf49a49537db
Joao Castro <joao.castro@terceiroslinx.com.br>
2020-06-11 17:36:06 -0300

    Init project base

---
+++ src/test/java/com/thejaum/challenge/sandwich/SandwichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejaum.challenge.sandwich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandwichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
